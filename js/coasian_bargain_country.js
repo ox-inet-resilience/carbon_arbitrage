54901700eba8217c4320baf87f137522006a0c3c
climatefinance <105055284+climatefinance@users.noreply.github.com>
2022-11-09 19:36:02 -0500

    Add Coasian bargain, country-level

---
+++ js/coasian_bargain_country.js
@@ -0,0 +1,12 @@
+import {levelDevelopmentMap, byRegionMap} from "./countries_grouping.js"
+
+export async function main() {
+  const dataRequest = await fetch("/public/country_specific_data_part6.json")
+  const data = await dataRequest.json()
+
+  const jsonData = JSON.stringify(data)
+  document.getElementById("result-data").innerHTML = jsonData
+  const downloadElement = document.getElementById("download-result-data")
+  downloadElement.href = "data:x-application/xml;charset=utf-8," + escape(jsonData)
+  downloadElement.download = `coasian_bargain_country.json`
+}
